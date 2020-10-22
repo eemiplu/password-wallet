62f1ccdc95969a252aea5b89d5172774dc86fd17
Emilia <emila1885@gmail.com>
2020-10-22 22:34:14 +0200

    First commit: created login window and MS database model with code-first

---
+++ PasswordWallet/DbModels/Password.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasswordWallet.DbModels
+{
+    [Table("Passwords")]
+    public class Password
+    {
+        [Key]
+        [Required]
+        [Column("Id", Order = 0)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("PasswordHash", Order = 1)]
+        public string PasswordHash { get; set; }
+
+        [Required]
+        [Column("IdUser", Order = 2)]
+        [ForeignKey("User")]
+        public int IdUser { get; set; }
+
+        [Column("WebAddress", Order = 3)]
+        public string WebAddress { get; set; }
+
+        [Column("Description", Order = 4)]
+        public string Description { get; set; }
+
+        [Column("Login", Order = 5)]
+        public string Login { get; set; }
+
+        public User User { get; set; }
+    }
+}
---
+++ PasswordWallet/DbModels/User.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PasswordWallet.DbModels
+{
+    [Table("Users")]
+    public class User
+    {
+        [Key]
+        [Required]
+        [Column("Id", Order = 0)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Login", Order = 1)]
+        public string Login { get; set; }
+
+        [Required]
+        [Column("PasswordHash", Order = 2)]
+        public string PasswordHash { get; set; }
+
+        [Column("Salt", Order = 3)]
+        public string Salt { get; set; }
+
+        [Column("IsPasswordStoredAsHash", Order = 4)]
+        public bool IsPasswordStoredAsHash { get; set; }
+
+        public ICollection<Password> Passwords { get; set; }
+    }
+}
